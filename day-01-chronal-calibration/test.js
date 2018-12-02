7ea366e14f9a36e728714806727a77ce11cea35e
Steven Horsman <steven@uk.ibm.com>
2018-12-02 17:52:04 +0000

    linting

--- day-01-chronal-calibration/test.js
+++ day-01-chronal-calibration/test.js
@@ -20,7 +20,7 @@ describe('Day 1: Chronal Calibration', () => {
        +1
        +1`;
 
-       expect(chronalCalibration.calculateFrequency(changes)).to.equal(3);
+    expect(chronalCalibration.calculateFrequency(changes)).to.equal(3);
   });
 
   it('should calculate frequency from +1, +1, -2', () => {
@@ -29,7 +29,7 @@ describe('Day 1: Chronal Calibration', () => {
        +1
        -2`;
 
-       expect(chronalCalibration.calculateFrequency(changes)).to.equal(0);
+    expect(chronalCalibration.calculateFrequency(changes)).to.equal(0);
   });
 
   it('should calculate frequency from -1, -2, -3', () => {
@@ -47,60 +47,60 @@ describe('Day 1: Chronal Calibration', () => {
   });
 });
 
-  describe('Part Two', () => {
-    it('should calculate first duplicate frequency from +1, -2, +3, +1', () => {
-      const changes =
+describe('Part Two', () => {
+  it('should calculate first duplicate frequency from +1, -2, +3, +1', () => {
+    const changes =
         `+1
          -2
          +3
          +1`;
 
-      expect(chronalCalibration.calculateFrequencyFoundTwice(changes)).to.equal(2);
-    });
+    expect(chronalCalibration.calculateFrequencyFoundTwice(changes)).to.equal(2);
+  });
 
-    it('should calculate first duplicate frequency from +1, -1', () => {
-      const changes =
+  it('should calculate first duplicate frequency from +1, -1', () => {
+    const changes =
         `+1
          -1`;
 
-        expect(chronalCalibration.calculateFrequencyFoundTwice(changes)).to.equal(0);
-    });
+    expect(chronalCalibration.calculateFrequencyFoundTwice(changes)).to.equal(0);
+  });
 
-    it('should calculate first duplicate frequency from +3, +3, +4, -2, -4', () => {
-      const changes =
+  it('should calculate first duplicate frequency from +3, +3, +4, -2, -4', () => {
+    const changes =
         `+3
          +3
          +4
          -2
          -4`;
 
-         expect(chronalCalibration.calculateFrequencyFoundTwice(changes)).to.equal(10);
-    });
+    expect(chronalCalibration.calculateFrequencyFoundTwice(changes)).to.equal(10);
+  });
 
-    it('should calculate first duplicate frequency from -6, +3, +8, +5, -6', () => {
-      const changes =
+  it('should calculate first duplicate frequency from -6, +3, +8, +5, -6', () => {
+    const changes =
         `-6
          +3
          +8
          +5
          -6`;
 
-         expect(chronalCalibration.calculateFrequencyFoundTwice(changes)).to.equal(5);
-    });
+    expect(chronalCalibration.calculateFrequencyFoundTwice(changes)).to.equal(5);
+  });
 
-    it('should calculate first duplicate frequency from +7, +7, -2, -7, -4', () => {
-      const changes =
+  it('should calculate first duplicate frequency from +7, +7, -2, -7, -4', () => {
+    const changes =
         `+7
          +7
          -2
          -7
          -4`;
 
-         expect(chronalCalibration.calculateFrequencyFoundTwice(changes)).to.equal(14);
-    });
-
-    it('should calculate first duplicate frequency from Input file', () => {
-      const input = fs.readFileSync('day-01-chronal-calibration/input.txt').toString();
-      expect(chronalCalibration.calculateFrequencyFoundTwice(input)).to.equal(709);
-    });
+    expect(chronalCalibration.calculateFrequencyFoundTwice(changes)).to.equal(14);
   });
+
+  it('should calculate first duplicate frequency from Input file', () => {
+    const input = fs.readFileSync('day-01-chronal-calibration/input.txt').toString();
+    expect(chronalCalibration.calculateFrequencyFoundTwice(input)).to.equal(709);
+  });
+});
--- day-02-inventory-management-system/inventory-management.js
+++ day-02-inventory-management-system/inventory-management.js
@@ -1,61 +1,58 @@
 function calculateChecksum(input) {
-    const boxIds = input
-        .split('\n')
-        .map((x) => x.trim());
+  const boxIds = input
+    .split('\n')
+    .map((x) => x.trim());
 
-    let twoTimes = 0;
-    let threeTimes = 0;
+  let twoTimes = 0;
+  let threeTimes = 0;
 
-    boxIds.forEach(id => {
-        let map = new Map();
-        id.split('').forEach(char => {
-            if (map.has(char)) {
-                map.set(char, map.get(char)+1);
-            } else {
-                map.set(char, 1); 
-            }
-        });
-
-        const counts = Array.from(map.values());
-        let found2 = Object.values(counts).includes(2);
-        let found3 = counts.filter(count => count === 3).length > 0;
-
-        if (found2) {
-            twoTimes++;
-        }
-        if (found3) {
-            threeTimes++;
-        }
+  boxIds.forEach(id => {
+    let map = new Map();
+    id.split('').forEach(char => {
+      if (map.has(char)) {
+        map.set(char, map.get(char) + 1);
+      } else {
+        map.set(char, 1);
+      }
     });
-    return twoTimes * threeTimes;
+
+    const counts = Array.from(map.values());
+    let found2 = Object.values(counts).includes(2);
+    let found3 = counts.filter(count => count === 3).length > 0;
+
+    if (found2) {
+      twoTimes++;
+    }
+    if (found3) {
+      threeTimes++;
+    }
+  });
+  return twoTimes * threeTimes;
 }
 
 function findCommonLetters(input) {
 
-    const ids = input
-        .split('\n')
-        .map((x) => x.trim());
+  const ids = input
+    .split('\n')
+    .map((x) => x.trim());
 
-    for (let i = 0; i < ids.length; i++) {
-        for (let j = 0; j < ids.length; j++) {
-            //Calculate number of diffs
-            let diffs = 0;
-            for (var x = 0; x < ids.length && diffs < 2; x++) {
-                if (ids[i].charAt(x) != ids[j].charAt(x)) {
-                    diffs++;
-                }
-            }
-            if (diffs == 1) {
-                let match = "";
-                for (var x = 0; x < ids.length && diffs < 2; x++) {
-                    if (ids[i].charAt(x) == ids[j].charAt(x)) {
-                        match = match + ids[i].charAt(x);
-                    }
-                }
-                return match;
-            }
+  for (let i = 0; i < ids.length; i++) {
+    for (let j = 0; j < ids.length; j++) {
+      //Calculate number of diffs
+      let diffs = 0;
+      let common = '';
+      for (var x = 0; x < ids.length && diffs < 2; x++) {
+        if (ids[i].charAt(x) != ids[j].charAt(x)) {
+          diffs++;
+        } else {
+          common = common + ids[i].charAt(x);
         }
+      }
+      if (diffs == 1) {
+        return common;
+      }
     }
+  }
 }
 
 module.exports.calculateChecksum = calculateChecksum;
