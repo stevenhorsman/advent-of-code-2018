d7c3ec502e9530785b9e2c8d0cd7da0dd81bf1cd
stevenhorsman <steven@uk.ibm.com>
2018-12-05 12:19:22 +0000

    Tidy up and refactor

--- day-05-alchemical-reduction/polymer.js
+++ day-05-alchemical-reduction/polymer.js
@@ -1,43 +1,19 @@
 function calculateReductionLength(input) {
-
-  let chars = input.split("");
-
-  //console.log(chars.join(""),"length",chars.length);
-
-  let prevChar = chars[0];
-  for (let i = 0; i< chars.length-1;i++) {
-    //console.log("chars",chars.join(""), "i",i);
-    if(chars[i] == null) {
-      console.log("error at char",i);
+  for (let i = 0; i< input.length-1;i++) {
+    if (Math.abs(input.charCodeAt(i) - input.charCodeAt(i+1)) === 32) {
+      input = input.substr(0,i) + input.substr(i+2);
+      i=i-2;
     }
-    let val_0 = chars[i].charCodeAt(0);
-    let val_1 = chars[i+1].charCodeAt(0);
-    if (Math.abs(val_0 - val_1) == 32) {
-      chars.splice(i,2);
-      i--;
-      if (i > -1) {
-        //if not first char then remove
-        i--;
-      }
-      //console.log("chars",chars.join(""), "i",i);
-    }
-
   }
-  //console.log(input,"->",chars.join(""),"=",chars.length);
-  return chars.length;
+  return input.length;
 }
 
 function calculateReductionLengthWithRemoval(input) {
 
-  minReductionLength = Array(26).fill().map((_, i) => {
-    let char = String.fromCharCode('A'.charCodeAt(0) + i);
-    inputcopy = input.replace(new RegExp(""+char, 'g'),"");
-    char = String.fromCharCode('a'.charCodeAt(0) + i);
-    inputcopy = inputcopy.replace(new RegExp(""+char, 'g'),"");
-    //console.log(input,"->",inputcopy,"replacing",char);
-    //inputcopy = inputcopy.replace(String.fromCharCode('a'.charCodeAt(0) + i,""));
-    return calculateReductionLength(inputcopy);
-  }).sort((a, b) => a -b )[0];
+  const minReductionLength = Array(26).fill()
+    .map((_, i) => input.replace(new RegExp(String.fromCharCode(65 + i), 'gi'),'')) //i ignores case
+    .map((input) => calculateReductionLength(input))
+    .sort((a, b) => a - b )[0];
   return minReductionLength;
 }
 
