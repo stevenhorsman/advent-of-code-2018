273ea70bf6f7bea265bcfa4f9278dda1146ff5bc
stevenhorsman <steven@uk.ibm.com>
2018-12-11 14:46:48 +0000

    Day 11 - poor performance

--- day-11-chronal-change/fuelcells.js
+++ day-11-chronal-change/fuelcells.js
@@ -1,5 +1,58 @@
+class FuelCellGrid {
+  constructor(serialNo, length = 300, subsectionSize = 3) {
+    this.serial = serialNo;
+    this.length = length;
+    this.subsectionSize = subsectionSize;
+    this.grid = Array
+      .from({ length })
+      .map(() => Array
+        .from({ length }));
+    for(let x = 0; x < 300; x++) {
+      for(let y = 0; y < 300; y++) {
+        this.grid[x][y] = calculateFuelCellValue(this.serial, x+1, y+1);
+        if(x >=32 && x<=34 && y>=44 && y <= 46) {
+          // console.log(calculateFuelCellValue(this.serial, x+1, y+1),'=',this.getValue(x,y));
+        }
+      //getGridValue(grid,32,44);
+      }
+    }
+  }
+
+  getMaxSubSectionCoordinate(size=this.subsectionSize) {
+    let currMax = -Infinity;
+    let coord;
+    for(let x = 0; x < this.length-size; x++) {
+      for(let y = 0; y < this.length-size; y++) {
+        let curr = this.sumSubSection(x, y,size);
+        if (curr > currMax) {
+          currMax = curr;
+          coord = `${x+1},${y+1}`;
+        }
+      }
+    }
+    return {coord:coord, power:currMax};
+  }
+
+  sumSubSection(xStart, yStart,size=this.subsectionSize) {
+    let sum = 0;
+    for(let x = 0; x < size; x++) {
+      for(let y = 0; y < size; y++) {
+        sum += this.grid[x+xStart][y+yStart];
+      }
+    }
+    return sum;
+  }
+
+  getValue(x,y) {
+    let value = this.grid[x][y];
+    console.log(`${x+1},${y+1}=${value}`);
+    return value;
+  }
+
+}
+
 function calculateFuelCellValue(serial, x, y) {
-  const rackId = (x + 10)
+  const rackId = (x + 10);
   let workingResult = ((rackId * y) + serial) * rackId;
   return getHundredsValue(workingResult) - 5;
 }
@@ -9,103 +62,24 @@ function getHundredsValue(input) {
   return (workingResult - (workingResult % 100)) / 100;
 }
 
+function calculateMaxFuelGrid3(serial) {
+  const grid = new FuelCellGrid(serial);
+  return grid.getMaxSubSectionCoordinate().coord;
+}
+
 function calculateMaxFuelGrid(serial) {
+  const grid = new FuelCellGrid(serial);
 
+  const maxPowers = [];
+  for(let i = 1; i <= 300; i++) {
+    let max_i = grid.getMaxSubSectionCoordinate(i);
+    maxPowers.push({size:i,coord:max_i.coord, power:max_i.power});
+  }
+  let max = maxPowers.sort((a, b) => b.power - a.power)[0];
+
+  return max.coord + ',' + max.size;
 }
-
-function calculateHighScore(input, inputFactor = 1) {
-  let result = input.match(/(\d+) players; last marble is worth (\d+) points/);
-  const playerCount = result[1];
-  const lastMarble = result[2] * inputFactor;
-
-  let board = [0];
-  let marbleIndex = 1;
-  const players = {};
-  for (let i = 0; i < playerCount; i += 1) {
-    players[i] = 0;
-  }
-  
-  let p = 0;
-  for(let m = 1; m <= lastMarble; m++) {
-    if (m % 23 == 0) {
-      let playerScore = m;
-      marbleIndex = (marbleIndex - 7 + board.length) % board.length;
-      playerScore += board.splice(marbleIndex,1)[0];
-      //console.log(p+1,"+",playerScore);
-      players[p] = players[p] + playerScore;
-    } else {
-      marbleIndex = (marbleIndex + 1) % board.length + 1;
-      board.splice(marbleIndex,0,m);
-    }
-    p = (p + 1) % playerCount;
-    //console.log('[',p+1,']', board.join(' '),'curr:', board[marbleIndex]);
-  }
-  return Math.max(...Object.values(players));
-}
-
-class Node {
-  constructor(data, prev, next) {
-    this.data = data;
-    this.prev = prev;
-    this.next = next;
-  }
-}
-function calculateHighScoreLL(input, inputFactor = 1) {
-  let result = input.match(/(\d+) players; last marble is worth (\d+) points/);
-  const playerCount = result[1];
-  const lastMarble = result[2] * inputFactor;
-
-  let currentNode = new Node(0);
-  currentNode.next = currentNode;
-  currentNode.prev = currentNode;
-
-  //const zeroPointer = currentNode;
-
-  const players = {};
-  for (let i = 0; i < playerCount; i += 1) {
-    players[i] = 0;
-  }
-  
-  let p = 0;
-  for(let m = 1; m <= lastMarble; m++) {
-    if (m % 23 == 0) {
-      let playerScore = m;
-      //console.log("cN", currentNode.data);
-      //console.log("cN.prev", currentNode.prev.data);
-      let remove = currentNode.prev.prev.prev.prev.prev.prev.prev;
-      //console.log("About to remove", remove.data);
-      currentNode = remove.next;
-      playerScore += remove.data;
-      currentNode.prev = remove.prev;
-      remove.prev.next = currentNode;
-      //console.log(p+1,"+",playerScore);
-      players[p] = players[p] + playerScore;
-    } else {
-      //console.log(m,':',currentNode.data);
-      const insert = new Node(m,currentNode.next, currentNode.next.next);
-      //console.log('inserting',insert.data, "[",insert.prev.data,insert.next.data,'}')
-      //console.log(currentNode.next.data,'.next',m);
-      //console.log(currentNode.next.next.data,'.prev',m);
-      currentNode.next.next.prev = insert;
-      currentNode.next.next = insert;
-      currentNode = insert;
-    }
-    p = (p + 1) % playerCount;
-    //console.log('[',p+1,']', listToArray(zeroPointer).join(' '),'curr:', currentNode.data,"[",currentNode.prev.data,currentNode.next.data,'}');
-  }
-  return Math.max(...Object.values(players));
-}
-
-// function listToArray(zeroPointer) {
-//   let current = zeroPointer;
-//   let data = [];
-//   do {
-//     data.push(current.data);
-//     current = current.next;
-//   }
-//   while (current != zeroPointer);
-//   return data;
-// }
 
 module.exports.calculateFuelCellValue = calculateFuelCellValue;
+module.exports.calculateMaxFuelGrid3 = calculateMaxFuelGrid3;
 module.exports.calculateMaxFuelGrid = calculateMaxFuelGrid;
--- day-11-chronal-change/test.js
+++ day-11-chronal-change/test.js
@@ -4,7 +4,7 @@ const fuelcells = require('./fuelcells');
 
 describe('Day 11: Chronal Charge', () => {
     
-  describe.only('Calculate fuel cell value', () => {
+  describe('Calculate fuel cell value', () => {
     it('fuel cell at 3,5 in a grid with serial number 8', () => {
       test(4, 8, 3, 5);
     });
@@ -28,53 +28,41 @@ describe('Day 11: Chronal Charge', () => {
   });
 
   describe('Part One', () => {
+    // it('print grid 33,45, serial 18', () => {
+    //   let serial = 18;
+    //   for(let y = 45;y<45+3;y++) {
+    //     let row = [];
+    //     for(let x = 33;x<33+3;x++) {
+    //       row.push(fuelcells.calculateFuelCellValue(serial, x, y));
+    //     }
+    //     console.log(row.join(' '));
+    //   }
+    // });
+
     it('should get x,y of largest total power for serial number 18', () => {
-      expect(fuelcells.calculateMaxFuelGrid(18)).to.equal('33,45');
+      expect(fuelcells.calculateMaxFuelGrid3(18)).to.equal('33,45');
     });
   
     it('should get x,y of largest total power for serial number 42', () => {
-      expect(fuelcells.calculateMaxFuelGrid(42)).to.equal('21,61');
+      expect(fuelcells.calculateMaxFuelGrid3(42)).to.equal('21,61');
     });
     
     it('should get x,y of largest total power for serial number 2187', () => {
-      expect(fuelcells.calculateMaxFuelGrid(2187)).to.equal('21,61');
+      expect(fuelcells.calculateMaxFuelGrid3(2187)).to.equal('235,85');
     });
   });
 
-  // describe('Part Two', () => {
-  //   it('should calculate high score for 9 players; last marble is worth 25 points', () => {
-  //     const input = '9 players; last marble is worth 25 points';
-  //     expect(marbles.calculateHighScoreLL(input)).to.equal(32);
-  //   });
-    
-  //   it('should calculate high score for 10 players; last marble is worth 1618 points', () => {
-  //     const input = '10 players; last marble is worth 1618 points';
-  //     expect(marbles.calculateHighScoreLL(input)).to.equal(8317);
-  //   });
+  describe.only('Part Two', () => {
+    it('should get x,y of largest total power for serial number 18', () => {
+      expect(fuelcells.calculateMaxFuelGrid(18)).to.equal('90,269,16');
+    }).timeout(500000);
 
-  //   it('should calculate high score for 13 players; last marble is worth 7999 points', () => {
-  //     const input = '13 players; last marble is worth 7999 points';
-  //     expect(marbles.calculateHighScoreLL(input)).to.equal(146373);
-  //   });
-  
-  //   it('should calculate high score for 17 players; last marble is worth 1104 points', () => {
-  //     const input = '17 players; last marble is worth 1104 points';
-  //     expect(marbles.calculateHighScoreLL(input)).to.equal(2764);
-  //   });
-  
-  //   it('should calculate high score for 21 players; last marble is worth 6111 points', () => {
-  //     const input = '21 players; last marble is worth 6111 points';
-  //     expect(marbles.calculateHighScoreLL(input)).to.equal(54718);
-  //   });
-  
-  //   it('should calculate high score for 30 players; last marble is worth 5807 points', () => {
-  //     const input = '30 players; last marble is worth 5807 points';
-  //     expect(marbles.calculateHighScoreLL(input)).to.equal(37305);
-  //   });
+    it('should get x,y of largest total power for serial number 42', () => {
+      expect(fuelcells.calculateMaxFuelGrid(42)).to.equal('232,251,12');
+    }).timeout(500000);
 
-  //   it('Input file should return', () => {
-  //     const input = fs.readFileSync('day-09-marble-mania/input.txt').toString();
-  //     expect(marbles.calculateHighScoreLL(input,100)).to.equal(2954067253);
-  //   });
-  // });
+    it('should get x,y of largest total power for serial number 2187', () => {
+      expect(fuelcells.calculateMaxFuelGrid(2187)).to.equal('233,40,13');
+    }).timeout(500000);
+  });
 });
