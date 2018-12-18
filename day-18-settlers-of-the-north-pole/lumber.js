2726b7e29f694233f069d2936ff56a9af4dda3ef
Steven Horsman <steven@uk.ibm.com>
2018-12-18 11:26:28 +0000

    Day 18

--- day-18-settlers-of-the-north-pole/lumber.js
+++ day-18-settlers-of-the-north-pole/lumber.js
@@ -16,29 +16,27 @@ Acre.initEnum({
   }
 });
 
-class Point {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-}
-
 function parse(char) {
   for (const d of Acre.enumValues) {
-    if(d.symbol === char) {
+    if (d.symbol === char) {
       return d;
     }
   }
+}
 
 class Grid {
-  constructor(input) {
-    let splits = input.split(/\r?\n/)
-      .map((line) => line = line.trim());
-    this.height = splits.length;
-    this.width = splits[0].length;
+  constructor(width, height) {
+    this.height = height;
+    this.width = width;
     this.grid = Array(this.height);
     for (let j = 0; j < this.height; j++) {
-        this.grid[j] = splits[j].split('').map(char => parse(char));
+      this.grid[j] = new Array(this.width);
+    }
+  }
+
+  fill(inputLines) {
+    for (let j = 0; j < this.height; j++) {
+      this.grid[j] = inputLines[j].split('').map(char => parse(char));
     }
   }
 
@@ -60,6 +58,59 @@ class Grid {
       console.log(this.grid[y].map(square => square.symbol).join(''));
     }
     console.log('');
+  }
+
+  evolve() {
+    let nextState = new Grid(this.width, this.height);
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        switch (this.get(x, y)) {
+          case Acre.OPEN: {
+            let woods = this.getNeighbourCount(x, y, Acre.WOOD);
+            if (woods >= 3) {
+              nextState.set(x, y, Acre.WOOD);
+            } else {
+              nextState.set(x, y, Acre.OPEN);
+            }
+            break;
+          }
+          case Acre.WOOD: {
+            let lumberyards = this.getNeighbourCount(x, y, Acre.LUMBERYARD);
+            if (lumberyards >= 3) {
+              nextState.set(x, y, Acre.LUMBERYARD);
+            } else {
+              nextState.set(x, y, Acre.WOOD);
+            }
+            break;
+          }
+          case Acre.LUMBERYARD: {
+            let woods = this.getNeighbourCount(x, y, Acre.WOOD);
+            let lumberyards = this.getNeighbourCount(x, y, Acre.LUMBERYARD);
+            if (woods > 0 && lumberyards > 0) {
+              nextState.set(x, y, Acre.LUMBERYARD);
+            } else {
+              nextState.set(x, y, Acre.OPEN);
+            }
+            break;
+          }
+        }
+      }
+    }
+    return nextState;
+  }
+
+  getNeighbourCount(x, y, type) {
+    let count = 0;
+    for (let i = x - 1; i <= x + 1; i++) {
+      for (let j = y - 1; j <= y + 1; j++) {
+        if (!(i == x && j == y)) { //don't count self
+          if (this.get(i, j) == type) {
+            count++;
+          }
+        }
+      }
+    }
+    return count;
   }
 
   get woods() {
@@ -88,87 +139,33 @@ class Grid {
 }
 
 function calculateResourceProduct(input, iterations = 10) {
-  let grid = new Grid(input);
-  grid.print();
-  //floodGrid(input,iterations);
+  let inputLines = input.split(/\r?\n/)
+    .map((line) => line = line.trim());
+  let grid = new Grid(inputLines[0].length, inputLines.length);
+  grid.fill(inputLines);
+  let cycleSearch = iterations > 500;
+  const results = [];
+  let prev = -1;
+  let result = grid.woods * grid.lumberYards;
+  results.push(result);
+  for (let i = 1; i <= iterations; i++) {
+    grid = grid.evolve();
+    if (cycleSearch) {
+      prev = result;
+      result = grid.woods * grid.lumberYards;
+      const prevIndex = results.indexOf(prev);
+      if (prevIndex != -1 && results.indexOf(result) == prevIndex + 1) {
+        const cycle = (i - prevIndex) - 1;
+        while (i < iterations - cycle) {
+          i += cycle;
+        }
+        cycleSearch = false;
+      } else {
+        results.push(result);
+      }
+    }
+  }
   return grid.woods * grid.lumberYards;
 }
 
-function floodGrid(input) {
-  let clay = [];
-  input.split(/\r?\n/)
-    .map((line) => {
-      line = line.trim();
-      let result = line.match(/([x|y])=(\d+), ([x|y])=(\d+)..(\d+)/);
-      if (result[1] == 'x') {
-        for (let i = +result[4]; i <= +result[5]; i++) {
-          clay.push(new Point(result[2], i));
-        }
-      } else {
-        for (let i = +result[4]; i <= +result[5]; i++) {
-          clay.push(new Point(i, +result[2]));
-        }
-      }
-    });
-
-  const sortX = clay
-    .map(p => p.x)
-    .sort((a, b) => b - a);
-  const minX = +sortX[sortX.length - 1] -1;
-  const width = (sortX[0] - minX) + 3;
-  const springX = 500 - minX;
-
-  const sortY = clay
-    .map(p => p.y)
-    .sort((a, b) => b - a);
-  const minY = +sortY[sortY.length - 1];
-  const height = (sortY[0] - minY) + 1;
-
-  // console.log('Grid from', minX, minY, ' to ', minX + width, minY + height);
-
-  let grid = new Grid(width, height, springX, Acre.SAND);
-  clay.forEach(pos => grid.set(pos.x - minX, pos.y - minY, Acre.CLAY));
-  //grid.print();
-
-  propagateWater(grid, springX, -1);
-  //grid.print();
-  return grid;
-}
-
-function propagateWater(grid, x, y) {
-  // console.log('propagateWater', x, y);
-  //grid.print();
-  //console.log('checking below',grid.get(x, y + 1));
-  let below = grid.get(x, y + 1);
-  if (below == Acre.SAND) {
-    // console.log('settingBelowToRunning');
-    //flow down and continue;
-    grid.set(x, y + 1, Acre.RUNNING);
-    propagateWater(grid, x, y + 1);
-  }
-  //console.log('below is Blocking',grid.get(x, y + 1));
-  if (grid.isBlocking(x, y + 1)) {
-    if (grid.get(x + 1, y) == Acre.SAND) {
-      // console.log('settingRightToRunning');
-      grid.set(x + 1, y, Acre.RUNNING);
-      propagateWater(grid, x + 1, y);
-    }
-  }
-  //console.log('below is Blocking2',grid.get(x, y + 1));
-  if (grid.isBlocking(x, y + 1)) {
-    if (grid.get(x - 1, y) == Acre.SAND) {
-      // console.log('settingLefttToRunning');
-      grid.set(x - 1, y, Acre.RUNNING);
-      propagateWater(grid, x - 1, y);
-    }
-  }
-  //console.log('getting bucket');
-  let range = grid.getBucketRange(x, y);
-  if (range[0] <= range[1]) {
-    // console.log('filling bucketRange', x, y, range);
-    for (let i = range[0]; i <= range[1]; i++) {
-      grid.set(i, y, Acre.WATER);
-    }
-  }
-}
 module.exports.calculateResourceProduct = calculateResourceProduct;
--- day-18-settlers-of-the-north-pole/test.js
+++ day-18-settlers-of-the-north-pole/test.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const lumber = require('./lumber');
 
-describe.only('Day 18: Settlers of The North Pole', () => {
+describe('Day 18: Settlers of The North Pole', () => {
     
   describe('Part One', () => {
     it('should calculate number of wet tiles', () => {
@@ -21,29 +21,17 @@ describe.only('Day 18: Settlers of The North Pole', () => {
       expect(lumber.calculateResourceProduct(input)).to.equal(1147);
     });
 
-    // it('Input file should return after 20 iterations', () => {
-    //   const input = fs.readFileSync('day-18-settlers-of-the-north-pole/input.txt').toString();
-    //   expect(lumber.calculateWetSquares(input)).to.equal(32552);
-    // }).timeout(10000);
+    it('Input file should return after 10 iterations', () => {
+      const input = fs.readFileSync('day-18-settlers-of-the-north-pole/input.txt').toString();
+      expect(lumber.calculateResourceProduct(input)).to.equal(588436);
+    });
   });
 
-  // describe('Part Two', () => {
-  //   it('should calculate number of wet tiles', () => {
-  //     const input = 
-  //     `x=495, y=2..7
-  //     y=7, x=495..501
-  //     x=501, y=3..7
-  //     x=498, y=2..4
-  //     x=506, y=1..2
-  //     x=498, y=10..13
-  //     x=504, y=10..13
-  //     y=13, x=498..504`;
-  //     expect(lumber.calculateStandingWater(input)).to.equal(29);
-  //   });
+  describe('Part Two', () => {
 
-  //   it('Input file should return after 20 iterations', () => {
-  //     const input = fs.readFileSync('day-18-settlers-of-the-north-pole/input.txt').toString();
-  //     expect(lumber.calculateStandingWater(input)).to.equal(26405);
-  //   }).timeout(10000);
-  // });
+    it('Input file should return after 1000000000 iterations', () => {
+      const input = fs.readFileSync('day-18-settlers-of-the-north-pole/input.txt').toString();
+      expect(lumber.calculateResourceProduct(input,1000000000)).to.equal(195290);
+    });
+  });
 });
